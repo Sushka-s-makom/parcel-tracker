61c90a2ce08fe2bc9d0e338d464771650cde1100
Sushka-s-makom <zabrodskiyalex111206@gmail.com>
2026-01-31 18:21:45 +0300

    chore : add new Handle func

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -19,6 +19,17 @@ func main() {
 		_, _ = fmt.Fprintln(w, "ok")
 	})
 
+	mux.HandleFunc("/tracks", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(http.StatusNotImplemented)
+		_, _ = w.Write([]byte(`{"error":"not implemented yet"}`))
+	})
+
 	srv := &http.Server{
 		Addr:              ":8080",
 		Handler:           mux,
