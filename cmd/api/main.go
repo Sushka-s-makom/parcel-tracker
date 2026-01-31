65717fcd32cd92c0a3b8449826c162fc784848c8
Sushka-s-makom <zabrodskiyalex111206@gmail.com>
2026-01-31 14:58:09 +0300

    feat: bootstrap http server with /health

---
+++ cmd/api/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+		w.Header().Set("Content-Type", "text/plain")
+		_, _ = fmt.Fprintln(w, "ok")
+	})
+
+	srv := &http.Server{
+		Addr:              ":8080",
+		Handler:           mux,
+		ReadHeaderTimeout: 5 * time.Second,
+	}
+	log.Println("listening on http://localhost:8080")
+	log.Fatal(srv.ListenAndServe())
+
+}
